35f68b4a314edc0cae74934251f2910e4fb707d4
CarolGP <cgpardal@gmail.com>
2026-02-20 21:00:36 +0100

    Planetamiento User.js bonus3

--- models/User.js
+++ models/User.js
@@ -1,4 +1,24 @@
-/**
- * User.js
- * Modelo de datos de usuarios
+/*User.js // Modelo de datos de usuarios
  */
+
+/* const mongoose = require ('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password:{
+        type: String,
+        require:true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default:'user'
+    }
+}, {timestamps:true});
+
+module.exports = mongoose.model('User', UserSchema); */
